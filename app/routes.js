fd142d5c758df1787980c0e7289d083226aaf325
Leonardo Pinheiro <leo.r.pinheiro@ulbra.edu.br>
2018-07-25 17:24:51 -0300

    finalization sign up, sign in, sign out and session control

--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const routes = express.Router();
 
-const authController = require('./controllers/authController');
+const authMiddleware = require('./middlewares/auth');
+const guestMiddleware = require('./middlewares/guest');
 
-routes.get('/', authController.signin);
-routes.get('/signup', authController.signup);
+const authController = require('./controllers/authController');
+const dashboardController = require('./controllers/dashboardController');
+
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success');
+  res.locals.flashError = req.flash('error');
+  next();
+});
+
+routes.get('/', guestMiddleware, authController.signin);
+routes.get('/signup', guestMiddleware, authController.signup);
+routes.get('/signout', authController.signout);
+
+routes.post('/register', authController.register);
+routes.post('/authenticate', authController.authenticate);
+
+routes.use('/app', authMiddleware);
+routes.get('/app/dashboard', dashboardController.index);
+
+routes.use((req, res) => res.render('errors/404'));
+
+routes.use((err, req, res, _next) => {
+  res.status(err.status || 500);
+
+  return res.render('errors/index', {
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
 
 module.exports = routes;
