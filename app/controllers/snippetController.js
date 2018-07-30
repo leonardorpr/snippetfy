f25901817f041248d118c504cbdd8be624465123
Leonardo Pinheiro <leo.r.pinheiro@ulbra.edu.br>
2018-07-30 13:46:41 -0300

    add (snippets) update

--- app/controllers/snippetController.js
+++ app/controllers/snippetController.js
@@ -37,12 +37,26 @@ module.exports = {
 
       const activeCategory = categoryId;
 
-      res.render('snippets/show', {
+      return res.render('snippets/show', {
         activeCategory,
         categories,
         snippets,
         currentSnippet,
       });
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  async update(req, res, next) {
+    try {
+      const snippet = await Snippet.findById(req.params.id);
+
+      await snippet.update(req.body);
+
+      req.flash('success', 'Snippet atualizado com sucesso!');
+
+      return res.redirect(`/app/categories/${req.params.categoryId}/snippets/${snippet.id}`);
     } catch (err) {
       return next(err);
     }
--- app/routes.js
+++ app/routes.js
@@ -35,6 +35,7 @@ routes.post('/app/categories/create', categoryController.store);
 // Snippets
 routes.get('/app/categories/:categoryId/snippets/:id', snippetController.show);
 routes.post('/app/categories/:categoryId/snippets/create', snippetController.store);
+routes.put('/app/categories/:categoryId/snippets/:id', snippetController.update);
 
 // Errors
 routes.use((req, res) => res.render('errors/404'));
